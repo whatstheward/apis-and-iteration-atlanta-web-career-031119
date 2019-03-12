4a163d183cd7fb814170cf0d30c722ad864cb34d
github username <github email address>
2019-03-12 15:16:23 -0400

    Maddie add print_movies method

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -26,12 +26,18 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
-  binding.pry
+  films.each do |info|
+    puts '/'*50
+    puts 'Title:' + info['title'] + ' Episode ' + info['episode_id'].to_s
+    puts 'Directed by: ' + info['director']
+    puts 'Released on ' + info['release_date']
+    puts " "
+    puts info['opening_crawl']
+end
 end
 
 def show_character_movies(character)
